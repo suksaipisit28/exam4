8ecdaa54b850eb2caec822d5b06bbe9e36afcfaf
pisit suksai <pisit_suksai@hotmail.com>
2024-05-17 10:42:01 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/exam4/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.exam4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/home_scree.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DBZ'),
+      ),
+      body: Column(children: [
+        Image.asset("assets/images/db.jpg"),
+      ]),
+    );
+  }
+}
