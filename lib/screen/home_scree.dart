68b89c344676f01ede89c213d4e3c0a5e38d967c
pisit suksai <pisit_suksai@hotmail.com>
2024-05-17 11:26:34 +0700

    check1

--- lib/screen/home_scree.dart
+++ lib/screen/home_scree.dart
@@ -14,9 +14,45 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('DBZ'),
       ),
-      body: Column(children: [
-        Image.asset("assets/images/db.jpg"),
-      ]),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Image.asset("assets/images/db.jpg"),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [Text("DBZ1"), Text("DBZ2")],
+                ),
+                Row(children: [
+                  Icon(Icons.star),
+                  Text("41"),
+                ])
+              ],
+            ),
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [Icon(Icons.call), Text("Call")],
+              ),
+              Column(
+                children: [Icon(Icons.route), Text("Route")],
+              ),
+              Column(
+                children: [Icon(Icons.share), Text("Share")],
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: const Text(
+                "Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentumAmbitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentumAmbitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum"),
+          )
+        ]),
+      ),
     );
   }
 }
